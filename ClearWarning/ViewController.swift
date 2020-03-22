57bfc352653ad6fe4c5d70775d67dbad63a3ca31
Alex <kjj9647@gmail.com>
2020-03-22 19:38:38 +0900

    Project initialized

---
+++ ClearWarning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClearWarning
+//
+//  Created by 행복한 개발자 on 2020/03/22.
+//  Copyright © 2020 Alex Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
